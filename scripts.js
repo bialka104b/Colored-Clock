79be2cc8275d2d01fc99b98c55a1d0688e520024
Marta Kulig <bialka104b@interia.pl>
2021-02-01 17:53:27 +0100

    added background

--- scripts.js
+++ scripts.js
@@ -8,10 +8,15 @@ function colorClock(){
     if (seconds < 10){seconds = '0' + seconds;} 
     if (minutes < 10){minutes = '0' + minutes;}
     var clockFace = hours+':'+minutes+':'+seconds;
-    var hexColor = '#' +seconds;  
+    // var hexColor = '#' + seconds + minutes + 50;  
+    const colorHex = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+
+    // console.log(Math.random()*16777215);
+    // console.log(Math.floor(Math.random()*16777215));
+    // console.log(`#${Math.floor(Math.random()*16777215).toString(16)}`);
 
     document.getElementById('clock').innerHTML = clockFace;
-    document.body.style.background = hexColor;
+    document.body.setAttribute('style', `background-color: ${colorHex};`);
 
     setTimeout(function() {
         colorClock();
